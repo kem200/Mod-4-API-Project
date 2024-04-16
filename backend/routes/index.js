5e40790354445bd13d2d2240f150badf6d5cf8f7
Kem Pizano <kempizano@gmail.com>
2024-04-15 21:03:32 -0700

    Removed test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
-
 
 
 router.get("/api/csrf/restore", (req, res) => {
