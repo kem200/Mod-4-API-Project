d14391f3853c54c59744ebdd41925d40eb6ecd43
Kem Pizano <kempizano@gmail.com>
2024-04-17 21:05:52 -0700

    Added firstName and lastName to database and updated routes to reflect changes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,44 +8,54 @@ const router = express.Router();
 
 const validateSignup = [
     check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
     check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
     check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
     check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a first name'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a last name'),
     handleValidationErrors
-  ];
+];
 
-  router.post(
+router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
-      const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+        const { email, password, username, firstName, lastName } = req.body;
+        const hashedPassword = bcrypt.hashSync(password);
+        const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-  
-      await setTokenCookie(res, safeUser);
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName
+        };
 
-      return res.json({
-        user: safeUser
-      });
+        await setTokenCookie(res, safeUser);
+
+        return res.json({
+            user: safeUser
+        });
     }
-  );
+);
 
 module.exports = router;
