ed59c01b1e9916525516c99279b2b53aa071cf7a
Kem Pizano <kempizano@gmail.com>
2024-04-25 23:34:00 -0700

    Added condition to omit stack in production environment for error responses

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
-const handleValidationErrors = (req, _res, next) => {
+const handleValidationErrors = (req, res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
@@ -11,13 +11,14 @@ const handleValidationErrors = (req, _res, next) => {
       .array()
       .forEach(error => errors[error.path] = error.msg);
 
+    console.log(isProduction)
     const err = Error("Bad request.");
     err.errors = errors;
     err.status = 400;
     err.title = "Bad request.";
-    
-    next(err);
+    next(err)
   }
+
   next();
 };
 
