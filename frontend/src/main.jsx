0461082732ead49cfe512bfc08150d16c2a753d0
Kem Pizano <kempizano@gmail.com>
2024-06-01 20:54:03 -0700

    Phase 2 authme complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,21 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+}
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 createRoot(document.getElementById('root')).render(
